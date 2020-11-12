ee0ace7fbba37e49d72fe591a90ef4c022c21f36
Antti Kantola <antti.kantola@helsinki.fi>
2020-11-12 21:53:51 +0200

    Exercise 1 done

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -1,7 +1,12 @@
 package ohtu;
 
+
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import org.apache.http.client.fluent.Request;
 
 public class Main {
@@ -9,16 +14,25 @@ public class Main {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
         
         String bodyText = Request.Get(url).execute().returnContent().asString();
-                
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
+        String nationality = "FIN";
+        
+//        System.out.println("json-muotoinen data:");
+//        System.out.println( bodyText );
 
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
         
-        System.out.println("Oliot:");
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter newTimeFormat = DateTimeFormatter.ofPattern("E MMM dd HH:mm:ss yyyy");
+        String formattedTime = time.format(newTimeFormat);
+        
+        System.out.println("Players form " + nationality + " " + formattedTime);
+        System.out.println("");
         for (Player player : players) {
-            System.out.println(player);
+            if (player.getNationality().equals(nationality)) {
+                System.out.println(player);
+            }
+            
         }   
     }
   
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,18 +3,39 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private String team;
+    private int assists;
+    private int goals;
 
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    
+    public void setAssists(int assists) {
+        this.assists = assists;
     }
 
     public String getName() {
         return name;
     }
+    
+    public String getNationality() {
+        return nationality;
+    }
+    
 
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists;
     }
       
 }
