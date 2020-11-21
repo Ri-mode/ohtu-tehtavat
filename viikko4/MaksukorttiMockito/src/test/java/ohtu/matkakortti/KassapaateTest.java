c5dc04c50724ff34973111ecb726d6c14fc375ad
Antti Kantola <antti.kantola@helsinki.fi>
2020-11-21 13:33:01 +0200

    Exercise 2 done

--- viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -29,7 +29,7 @@ public class KassapaateTest {
         kassa.ostaLounas(kortti);
         
         verify(kortti, times(1)).getSaldo();
-//        verify(kortti).osta(anyInt());
+        verify(kortti).osta(anyInt());
         verify(kortti).osta(eq(Kassapaate.HINTA));
         
     }
@@ -41,6 +41,21 @@ public class KassapaateTest {
         
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
+    }
+    
+    @Test
+    public void kortilleVoiLadataKunSummaPositiivinen() {
+        kassa.lataa(kortti, 10);
+        
+        verify(kortti).lataa(eq(10));
+        verify(kortti, times(1)).lataa(anyInt());
+    }
+    
+    @Test
+    public void kortilleEiVoiLadataKunSummaNegatiivinen() {
+        kassa.lataa(kortti, -10);
+        
+        verify(kortti, times(0)).lataa(anyInt());
     }
       
 }
